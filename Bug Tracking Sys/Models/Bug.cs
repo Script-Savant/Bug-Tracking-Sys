c78f03d5f594743b08af3da712835ba4ae50a100
Script-Savant <alexkienjeku@gmail.com>
2025-03-25 18:50:14 +0300

    adds models definition(user, bug & bughistory)

---
+++ Bug Tracking Sys/Models/Bug.cs
@@ -0,0 +1,39 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BugTrackingSys.Models
+{
+    public enum BugStatus
+    {
+        Open,
+        InProgress,
+        Resolved,
+        Closed
+    }
+
+    public class Bug
+    {
+        [Key]
+        public int BugId { get; set; }
+
+        [Required(ErrorMessage = "A title is required")]
+        [StringLength(200, ErrorMessage = "Title cannot be longer than 200 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "A description is required")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Created By is required")]
+        public User CreatedBy { get; set; }
+
+        public User AssignedTo { get; set; }
+
+        [Required(ErrorMessage = "Created At is required")]
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        public BugStatus Status { get; set; }
+    }
+}
---
+++ Bug Tracking Sys/Models/User.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BugTrackingSys.Models
+{
+    public enum UserRole
+    {
+        Admin,
+        Developer,
+        Tester
+    }
+
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "A name is required")]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "An Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A role is required")]
+        public UserRole Role { get; set; }
+
+        [Required(ErrorMessage = "A password is required")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long")]
+        public string PasswordHash { get; set; }
+    }
+}
